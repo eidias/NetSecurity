281a58cacb1b6e33fcdf7d35a83801ed10ea13fa
Sandro Galfo <sandro.galfo@eidias.ch>
2021-12-04 14:22:32 +0100

    Added certificate and signing tools.

---
+++ Tools/AddAuthenticodeSignature.ps1
@@ -0,0 +1,10 @@
+﻿param
+(
+[Parameter(Mandatory = $true)]
+[string]	      
+$FilePath
+)
+$password = ConvertTo-SecureString -String "1234" -Force -AsPlainText
+$cert = Get-PfxCertificate -FilePath ExampleCodeSigning.pfx
+$signature = Set-AuthenticodeSignature -FilePath $FilePath -Certificate $cert -TimestampServer http://timestamp.comodoca.com/authenticode
+Write-Host $signature.StatusMessage
